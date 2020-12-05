c9f492cbddc8cc0992bb3eb1651b010f9e46a77f
dezordi <zimmer.filipe@gmail.com>
2020-12-04 22:40:25 -0300

    update blog application with pagination function

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request): #will get all posts that have the 'published' status
-    post = Post.published.all()
-    return render(request,'blog/post/list.html', {'posts':post}) #and return the posts in the html page
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 5) # 5 posts will be showed in each page
+    page = request.GET.get('page') # get current page number
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,'blog/post/list.html', {'page':page,'posts':posts}) #and return the posts in the html page
 
 #Showing a single post
 def post_detail(request,year,month,day,post):
