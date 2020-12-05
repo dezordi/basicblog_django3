3be28816db8e9913149f8a2be558a034a23fe7d9
dezordi <zimmer.filipe@gmail.com>
2020-12-05 20:08:23 -0300

    Add basic user register with allauth

--- basicblog/settings.py
+++ basicblog/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'blog.apps.BlogConfig', #load our blog application configuration
     'taggit', #to use tags in our blog
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# django-allauth
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = "/blog"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
