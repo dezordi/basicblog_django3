db1989b14601bb455512624b5ea7bdd471fc94eb
dezordi <zimmer.filipe@gmail.com>
2020-12-05 13:39:44 -0300

    add module do comment function

--- blog/models.py
+++ blog/models.py
@@ -22,9 +22,24 @@ class Post(models.Model): #creating all fields related to a blog post, that will
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
-        
+
     class Meta: #to sort results by publishing field
         ordering = ('-publish',) # - indicates a descending order, so posts published recently will be showed first
 
     def __str__(self): #human readable string (in this case, title)
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') #associate the comment to a specific post
+    name = models.CharField(max_length = 120)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -25,4 +26,14 @@ def post_list(request): #will get all posts that have the 'published' status
 #Showing a single post
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year,publish__month=month,publish__day=day)
-    return render(request,'blog/post/detail.html',{'post':post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments, 'new_comment': new_comment, 'comment_form':comment_form})
